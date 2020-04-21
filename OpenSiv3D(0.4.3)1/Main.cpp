b196ce9fff938c2d64dc45897f6629505f4e170c
ikuramikan <haruao1124@icloud.com>
2020-04-21 21:18:02 +0900

    Periodic::Square0_1()

--- OpenSiv3D(0.4.3)1/Main.cpp
+++ OpenSiv3D(0.4.3)1/Main.cpp
@@ -3,56 +3,13 @@
 
 void Main()
 {
-	Scene::SetBackground(Palette::White);
-	const Vec2 center = Scene::Center();
-	Stopwatch stopwatch(true);
+	Scene::SetBackground(ColorF(0.25));
 	while (System::Update())
 	{
-		if (MouseL.down())
+		if (Periodic::Square0_1(2s))
 		{
-			if (stopwatch.isRunning())
-			{
-				stopwatch.pause();
-			}
-			else
-			{
-				stopwatch.resume();
-			}
+			Circle(Scene::Center(), 200).draw();
 		}
-		const double t = stopwatch.sF();
-		Circle(center, 120).drawArc(t * 140_deg, 240_deg, 60, 0, ColorF(0.4));
-		Circle(center, 180).drawArc(t * 90_deg, 160_deg, 60, 0, ColorF(0.6));
-		Circle(center, 240).drawArc(t * 50_deg, 120_deg, 60, 0, ColorF(0.8));
-
 	}
 }
 
-//
-// = アドバイス =
-// Debug ビルドではプログラムの最適化がオフになります。
-// 実行速度が遅いと感じた場合は Release ビルドを試しましょう。
-// アプリをリリースするときにも、Release ビルドにするのを忘れないように！
-//
-// 思ったように動作しない場合は「デバッグの開始」でプログラムを実行すると、
-// 出力ウィンドウに詳細なログが表示されるので、エラーの原因を見つけやすくなります。
-//
-// = お役立ちリンク =
-//
-// OpenSiv3D リファレンス
-// https://siv3d.github.io/ja-jp/
-//
-// チュートリアル
-// https://siv3d.github.io/ja-jp/tutorial/basic/
-//
-// よくある間違い
-// https://siv3d.github.io/ja-jp/articles/mistakes/
-//
-// サポートについて
-// https://siv3d.github.io/ja-jp/support/support/
-//
-// Siv3D ユーザコミュニティ Slack への参加
-// https://siv3d.github.io/ja-jp/community/community/
-//
-// 新機能の提案やバグの報告
-// https://github.com/Siv3D/OpenSiv3D/issues
-//
